e051525db565623970ccc2673a5795ff90902054
Omar <omardavidjm@ufps.edu.co>
2025-03-10 19:29:35 -0500

    feat: integrate Fastify adapter and update controller path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
