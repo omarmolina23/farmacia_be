7a08cfc19da6e9e7c295e4ec09ff91bfdbc8c782
Omar <omardavidjm@ufps.edu.co>
2025-03-23 16:14:13 -0500

    feat: integrate Swagger for API documentation and enhance user controller with Swagger decorators

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import fastifyCookie from '@fastify/cookie';
+import fastifySwagger from '@fastify/swagger'; // Importar fastify-swagger
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,22 +15,33 @@ async function bootstrap() {
     new FastifyAdapter()
   );
 
+  const globalPrefix = 'api';
   const port = process.env.PORT || 3000;
 
-  await app.register(fastifyCookie, {
+  const config = new DocumentBuilder()
+    .setTitle('Farmacia Nueva Esperanza')
+    .setDescription('Farmacia Nueva Esperanza API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addServer(`/${globalPrefix}`) 
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.register(fastifyCookie, {
     secret: process.env.SECRET,
   });
 
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
-  app.setGlobalPrefix('api');
-  
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix(globalPrefix);
+
   app.enableCors({
     origin: ['http://localhost:5173'], // Orígenes permitidos
     methods: 'GET,HEAD,PUT,PATCH,POST', // Métodos permitidos
     allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
     credentials: true, // Permitir credenciales (cookies, autorizaciones)
   });
-  
+
   await app.listen(port, '0.0.0.0');
 }
 
--- src/supplier/supplier.service.ts
+++ src/supplier/supplier.service.ts
@@ -78,6 +78,4 @@ export class SupplierService {
       throw error;
     }
   }
-
-
 }
