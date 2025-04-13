8265e0991dc7768c22bb3f7dc0e68994b2aaa5cf
Omar <omardavidjm@ufps.edu.co>
2025-04-12 23:18:07 -0500

    feat: add authentication and authorization guards to category and supplier controllers

--- src/supplier/supplier.controller.ts
+++ src/supplier/supplier.controller.ts
@@ -7,25 +7,35 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { SupplierService } from './supplier.service';
 import { CreateSupplierDto } from './dto/create.supplier';
 import { UpdateSupplierDto } from './dto/update.supplier';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { Roles } from 'src/auth/validators/roles.decorator';
 
 @Controller('supplier')
 export class SupplierController {
   constructor(private readonly supplierService: SupplierService) {}
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('admin')
   @Post()
   create(@Body() createSupplierDto: CreateSupplierDto) {
     return this.supplierService.create(createSupplierDto);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('admin')
   @Get()
   findAll() {
     return this.supplierService.findAll();
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('admin')
   @Get('search')
   search(
     @Query('query') query?: string,
@@ -33,6 +43,8 @@ export class SupplierController {
     return this.supplierService.findByNameOrEmail(query);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('admin')
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -41,6 +53,8 @@ export class SupplierController {
     return this.supplierService.update(id, updateSupplierDto);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('admin')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.supplierService.remove(id);
