9bbb3728d12956c8902cf4a0e7c54a204e1a3a37
Omar <omardavidjm@ufps.edu.co>
2025-05-15 17:34:52 -0500

    feat: add SalesController and SalesService to SalesModule

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,7 +1,10 @@
 import {Module} from '@nestjs/common';
 import { ScanGateway } from './scan.gateway';
+import { SalesService } from './sales.service';
+import { SalesController } from './sales.controller';
 
 @Module({
-    providers: [ScanGateway],
+    providers: [ScanGateway, SalesService],
+    controllers: [SalesController],
 })
 export class SalesModule {}
