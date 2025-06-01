1f7bcc12214a172235c68152f3322e8edda70f80
BryanDev <bryanalejandrovo@ufps.edu.co>
2025-06-01 09:34:21 -0500

    feat: add console log for date

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -371,6 +371,9 @@ export class SalesService {
     const { start: gte } = getStartEndOfDayInColombia(startDate);
     const { end: lte } = getStartEndOfDayInColombia(endDate);
 
+    console.log('Fecha de inicio:', gte);
+    console.log('Fecha de fin:', lte);
+
     if (repaid === undefined) {
       repaid = false;
     }
